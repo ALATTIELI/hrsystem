4e5a5b4cd53c340fb0d55625ac2542a5b9341afb
ADEL ALATTIELI <2010181@acuq.ae>
2023-08-08 18:21:29 +0400

    cart added 50% done

---
+++ src/pages/stockorder/cartcontext.tsx
@@ -0,0 +1,39 @@
+// cartcontext.tsx
+import { createContext, useContext, ReactNode, useState } from 'react';
+
+interface CartItem {
+  quantity: ReactNode;
+  photoUrl: string | undefined;
+  id: string;
+  name: string;
+  price: number;
+}
+
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export function CartProvider({ children }: { children: ReactNode }) {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prevCart) => [...prevCart, item]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCartContext() {
+  const context = useContext(CartContext);
+  if (context === undefined) {
+    throw new Error('useCartContext must be used within a CartProvider');
+  }
+  return context;
+}
