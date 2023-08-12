c061b5d0ffa4c8079aa0911826a6acd539ab5d6d
ADEL ALATTIELI <2010181@acuq.ae>
2023-08-12 23:38:46 +0400

    buttons modification

--- src/pages/employeelogin/buttonconfig.tsx
+++ src/pages/employeelogin/buttonconfig.tsx
@@ -1,29 +1,30 @@
 export const commonButtons = [
-    "Declaration Form",
-    "Request Certificates",
-    "Leave Request",
-    "Yearly Vacation Form"
-  ];
-  
-  export const positionButtons: { [key: string]: string[] } = {
-    "OPERATION MANAGER": [
-      "Operation Manager Checklist Report", 
-      "Rate the Employees", 
-      "Warning Letter Form"
-    ].concat(commonButtons),
-    
-    "BRANCH MANAGER": [
-      "Branches Manager Checklist Report", 
-      "Warning Letter Form", 
-      "Create Employees Weekly Tasks"
-    ].concat(commonButtons),
-    
-    "BRANCH SUPERVISOR": [
-      "Create the Weekly Schedule of Work for the Employees", 
-      "Branch Supervisor Checklist", 
-      "Warning Letter Form"
-    ].concat(commonButtons),
-    
-    "EXECUTIVE DIRECTOR": ["Feedback Reports"].concat(commonButtons)
-  };
-  
+  "Leave Request",
+  "Salary Certificate",
+  "Yearly Vacation Form",
+  "Experience Certificate",
+  "Vacation Allowance Request",
+];
+
+export const positionButtons: { [key: string]: string[] } = {
+  "OPERATION MANAGER": [
+    "Operation Manager Checklist Report",
+    "Rate the Employees",
+    "Warning Letter Issue Form",
+  ].concat(commonButtons),
+
+  "BRANCH MANAGER": [
+    "Branches Manager Checklist Report",
+    "Warning Letter Form",
+    "Create Employees Weekly Tasks",
+  ].concat(commonButtons),
+
+  "BRANCH SUPERVISOR": [
+    "Create the Weekly Schedule of Work for the Employees",
+    "Branch Supervisor Checklist",
+    "Warning Letter Issue Form",
+    "Appreciation Certificate",
+  ].concat(commonButtons),
+
+  "EXECUTIVE DIRECTOR": ["Feedback Reports"].concat(commonButtons),
+};
