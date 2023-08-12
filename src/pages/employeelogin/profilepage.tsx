4b4dd088093b90b0d190daf693f4af1ed0243523
ADEL ALATTIELI <2010181@acuq.ae>
2023-08-12 23:12:58 +0400

    ..

--- src/pages/employeelogin/profilepage.tsx
+++ src/pages/employeelogin/profilepage.tsx
@@ -14,10 +14,44 @@ function ProfilePage() {
 
   const [selectedForm, setSelectedForm] = useState<string | null>(null);
 
+  // Commenting out the unused state values:
+  // const [breakStartTime, setBreakStartTime] = useState<Date | null>(null);
+  // const [breakEndTime, setBreakEndTime] = useState<Date | null>(null);
+  const [isBreakStarted, setIsBreakStarted] = useState(false);
+
   const handleButtonClick = (formName: string) => {
-    console.log("Button clicked with name:", formName);  // This log remains the same.
+    console.log("Button clicked with name:", formName);  
     setSelectedForm(formName);
   };
+
+  const toggleBreak = () => {
+    if (!isBreakStarted) {
+      handleStartBreak();
+    } else {
+      handleEndBreak();
+    }
+    setIsBreakStarted(!isBreakStarted);
+  };
+
+  const handleStartBreak = () => {
+    const startTime = new Date();
+    // setBreakStartTime(startTime);
+    console.log(`Break started at ${startTime.toLocaleTimeString()}`);
+  };
+
+  const handleEndBreak = () => {
+    const endTime = new Date();
+    // setBreakEndTime(endTime);
+    console.log(`Break ended at ${endTime.toLocaleTimeString()}`);
+  };
+
+  const handleLogout = () => {
+    console.log(`Employee logged out at ${new Date().toLocaleTimeString()}`);
+    // Here you can also redirect the user to the login page or any other logic for logout
+    // Example: useHistory().push('/login');
+  };
+
+
 
   return (
     <div className="profile-container">
@@ -35,6 +69,19 @@ function ProfilePage() {
             <p>Position: {selectedEmployee?.position}</p>
           </div>
         </div>
+        {/* Add Start and End break buttons */}
+        <div className="profile-container">
+            {/* ... other divs */}
+            <div className="fixed-buttons">
+                <button onClick={toggleBreak}>
+                    {isBreakStarted ? "End Break" : "Start Break"}
+                </button>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+        </div>
+
+      {/* Logout Button */}
+      <button onClick={handleLogout}>Logout</button>
         <Link to="/employeelogin" className="back-button">
           <ArrowBackIcon className="arrow-icon" />
           Back
