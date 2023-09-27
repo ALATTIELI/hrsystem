3faa0ab022ffe3fa3528f776023924209b7343d2
bil0ak <bilal0akkil@gmail.com>
2023-09-28 03:14:15 +0400

    add persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 // main.tsx
-import React from 'react';
+import React from "react";
 import "./index.css";
-import ReactDOM from 'react-dom';
-import AppRoutes from './Routes'; // Import the renamed component
+import ReactDOM from "react-dom";
+import AppRoutes from "./Routes"; // Import the renamed component
 
 // Redux Imports
-import { Provider } from 'react-redux';
-import store from './redux/store';  // Assuming you've created a store.js or store.tsx file that exports your store.
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppRoutes />
+      <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
+        <AppRoutes />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
